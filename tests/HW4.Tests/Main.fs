8cf77ab2260c361c00ae31c652946c7bee6b3c75
Kirill Garbar <hailcaesar322@gmail.com>
2020-10-28 17:41:51 +0300

    HW4 some trash files deleted

--- tests/HW4.Tests/Main.fs
+++
@@ -1,8 +0,0 @@
-module ExpectoTemplate
-
-open Expecto
-
-let config = { FsCheckConfig.defaultConfig with maxTest = 1000 }
-
-[<EntryPoint>]
-let main argv = Tests.runTestsInAssembly defaultConfig argv
