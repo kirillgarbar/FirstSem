0098a8836b69a91e4f89ff877ac914f605ef2516
Kirill Garbar <hailcaesar322@gmail.com>
2021-02-07 01:58:48 +0300

    HW8 Main.fs fixed

--- src/HW8/Main.fs
+++ src/HW8/Main.fs
@@ -5,8 +5,4 @@ open MyList
 
 [<EntryPoint>]
 let main (argv: string array) =
-    let x = BigInt(1, Cons(8, Cons(8, Cons(1, Cons(0, One 6)))))
-    let y = BigInt(1, Cons(1, Cons(1, One 0)))
-    let s = div x y
-    printfn "%A" (s.Bits, s.Sign)
     0
