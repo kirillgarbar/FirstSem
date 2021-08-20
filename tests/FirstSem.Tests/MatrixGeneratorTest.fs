bf69f408ea768cf8fc3c92c09fc39a10d02f78e8
Kirill Garbar <st087492@student.spbu.ru>
2021-08-20 15:23:35 +0300

    Types for CI, test separation, optimization of generator and writing

--- tests/FirstSem.Tests/MatrixGeneratorTest.fs
+++ tests/FirstSem.Tests/MatrixGeneratorTest.fs
@@ -9,19 +9,23 @@ let readMatrix path = Array.map (fun (s:string) -> s.Split ' ') (IO.File.ReadAll
 [<Tests>]
 let tests =
     testList "Tests for MatrixGenetator" [
-    testProperty "write and read id test" <| fun _ ->
+    testProperty "write and read id test for Int matrices" <| fun _ ->
        let im = generateMatrix (Random().Next(1, 10)) (Random().Next(1, 10)) (Random().Next(101)) intGenerator
-       let bm = generateMatrix (Random().Next(1, 10)) (Random().Next(1, 10)) (Random().Next(101)) boolGenerator
-       let fm = generateMatrix (Random().Next(1, 10)) (Random().Next(1, 10)) (Random().Next(101)) floatGenerator
-       writeMatrix "GeneratorTest1.txt" im
-       writeMatrix "GeneratorTest2.txt" bm
-       writeMatrix "GeneratorTest3.txt" fm
-       let rim = readMatrix "GeneratorTest1.txt"
-       let rbm = readMatrix "GeneratorTest2.txt"
-       let rfm = readMatrix "GeneratorTest3.txt"
+       writeMatrix "GeneratorTestInt.txt" im
+       let rim = readMatrix "GeneratorTestInt.txt"
        Expect.equal rim im "write and read should be id"
-       Expect.equal rbm bm "write and read should be id"
-       Expect.equal rfm fm "write and read should be id"
+
+    testProperty "write and read id test for Bool matrices" <| fun _ ->
+        let bm = generateMatrix (Random().Next(1, 10)) (Random().Next(1, 10)) (Random().Next(101)) boolGenerator
+        writeMatrix "GeneratorTestBool.txt" bm
+        let rbm = readMatrix "GeneratorTestBool.txt"
+        Expect.equal rbm bm "write and read should be id"
+
+    testProperty "write and read id test for Float matrices" <| fun _ ->
+        let fm = generateMatrix (Random().Next(1, 10)) (Random().Next(1, 10)) (Random().Next(101)) floatGenerator
+        writeMatrix "GeneratorTestFloat.txt" fm
+        let rfm = readMatrix "GeneratorTestFloat.txt"
+        Expect.equal rfm fm "write and read should be id"
 
     testCase "Zero dimensional matrix test" <| fun _ ->
         Expect.throws (fun _ -> generateMatrix 0 5 10 intGenerator |> ignore) "Exception should be raised"
